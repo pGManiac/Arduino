5b519d6bbadf1ae4069f98eda6408d5e3ae398ba
pGManiac <konradkomplexeleistung@gmail.com>
2023-12-16 12:49:55 +0100

    First attempt at refactored SerialPort code

--- FullDuplex2/Computer/SerialPort.cpp
+++ FullDuplex2/Computer/SerialPort.cpp
@@ -4,7 +4,7 @@
 
 SerialPort::SerialPort(const char* _portName) {
     // Open the serial port
-    fd = open(_portName, O_RDWR | O_NOCTTY | O_NDELAY);
+    fd = open(_portName, O_RDWR | O_NOCTTY);
     if (fd == -1) {
         std::cerr << "Error opening serial port." << std::endl;
         // Handle the error
@@ -21,41 +21,11 @@ SerialPort::~SerialPort() {
 
 void SerialPort::configure() {
     struct termios serialConfig;
-
-    // Get the current serial port configuration
-    if (tcgetattr(fd, &serialConfig) != 0) {
-        std::cerr << "Error getting current serial port configuration.,\n";
-        return;
-    }
-
-    // Set the input and output baud rate to 9600
-    cfsetispeed(&serialConfig, B9600);
-    cfsetospeed(&serialConfig, B9600);
-
-    // Set data bits to 8
-    serialConfig.c_cflag &= ~CSIZE; //clears existing CSIZE bits
-    serialConfig.c_cflag |= CS8;
-
-    // Disable hardware flow control, aka RTS/CTS
-    serialConfig.c_cflag &= ~CRTSCTS;
-
-    // Set parity to no parity
-    serialConfig.c_cflag &= ~PARENB;
-    serialConfig.c_cflag &= ~CSTOPB;
-
-    // Disable software flow control
-    serialConfig.c_iflag &= ~(IXON | IXOFF | IXANY);
-
-    // Set raw input
-    serialConfig.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-    // Set raw output
-    serialConfig.c_oflag &= ~OPOST;
-
-    // Apply the configuration
-    if (tcsetattr(fd, TCSAFLUSH, &serialConfig) != 0) {
-        std::cerr << "Error configuring serial port.\n";
-    }
+    tcgetattr(fd, &serialConfig);
+    cfsetospeed(&serialConfig, 9600);
+    cfsetispeed(&serialConfig, 9600);
+    serialConfig.c_cflag |= (CLOCAL | CREAD);
+    tcsetattr(fd, TCSANOW, &serialConfig);
 }
 
 
@@ -68,40 +38,36 @@ void SerialPort::sendBytes(const uint8_t* data , size_t size) {
 void SerialPort::receive8Bytes() {
     bytesAvailable = ioctl(fd, FIONREAD, &bytesAvailable);
     //if(fcntl(fd, FIONREAD, &bytesAvailable))
-    if(bytesAvailable != 1) {
-        std::cout << bytesAvailable << "\n";
-        switch(bytesAvailable) {
-            case -1:
-                std::cerr << "Error checking bytes available in serial port.\n";
+    switch(bytesAvailable) {
+        case -1:
+            std::cerr << "Error checking bytes available in serial port.\n";
+            break;
+        case 0:
+            break;
+        default:
+            if(bytesAvailable >= 8) {
+                read(fd, &buffByte, 8);
+                bytesAvailable = 0;
+                bytesAvailableLast = 0;
+                availableBuffer = true;
                 break;
-            case 0:
-                break;
-            default:
-                if(bytesAvailable >= 8) {
-                    read(fd, &buffByte, 8);
-                    bytesAvailable = 0;
-                    bytesAvailableLast = 0;
-                    availableBuffer = true;
-                    break;
-                } else if (bytesAvailable > bytesAvailableLast){
-                    bytesAvailableLast = bytesAvailable;
-                    receive8Bytes();
-                } else {
-                    for (uint8_t & i : buffByte) {
-                        i = 0;
-                    }
-                    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
-                    char buffer[8];
-                    read(fd, buffer, sizeof(buffer));
-                    availableBuffer = true;
-                    bytesAvailable = 0;
-                    bytesAvailableLast = 0;
-                    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) & ~O_NONBLOCK);
-                    break;
+            } else if (bytesAvailable > bytesAvailableLast){
+                bytesAvailableLast = bytesAvailable;
+                receive8Bytes();
+            } else {
+                for (uint8_t & i : buffByte) {
+                    i = 0;
                 }
-        }
+                fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+                char trashbuffer[8];
+                read(fd, trashbuffer, sizeof(trashbuffer));
+                availableBuffer = true;
+                bytesAvailable = 0;
+                bytesAvailableLast = 0;
+                fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) & ~O_NONBLOCK);
+                break;
+            }
     }
-
 }
 
 void SerialPort::makeBufferNotAvailable() {
