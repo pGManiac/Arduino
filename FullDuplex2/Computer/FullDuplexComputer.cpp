a9c8ac22ea6161359c26a5cfe7e37d4f633deb6e
Myoshka <mishajusten@web.de>
2024-01-12 16:34:58 +0100

    changed input file reading to user input reading from terminal

--- FullDuplex2/Computer/FullDuplexComputer.cpp
+++ FullDuplex2/Computer/FullDuplexComputer.cpp
@@ -2,6 +2,8 @@
 
 #include "Queue.hpp"
 #include "SerialPort.hpp"
+#include <string>
+#include <iostream>
 
 const char* portName = "/dev/ttyUSB0";
 const char* inputFileName = "input.txt";
@@ -11,16 +13,29 @@ const char* outputFileName = "output.txt";
 int main() {
     Queues queues(portName);
 
-    queues.openInputFile(inputFileName);
+    //queues.openInputFile(inputFileName);
 
     usleep(5000000);
 
+    std::string eingabe;
+    uint8_t zeichen;
+    std::cout << "Eingabe: ";
+    std::getline(std::cin, eingabe);
+
+    for (int i = 0; i < eingabe.length(); i++) {
+        zeichen = eingabe.at(i);
+        queues.sendByte(zeichen);
+    }
+
+    /*
     char byteFromFile;
     while (queues.readByteFromFile(byteFromFile)) {
         queues.sendByte(static_cast<uint8_t>(byteFromFile));
     }
     queues.closeInputFile();
+    */
     queues.flush();
+
 
     while(true) {
         queues.send();
